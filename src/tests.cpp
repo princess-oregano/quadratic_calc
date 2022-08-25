210962cf713ecb2c2ce9035c7bb40cdbcc488df3
Anastasia <kulevich05@gmail.com>
2022-08-25 16:45:10 +0300

    Functions in tests.cpp specified as static.

--- src/tests.cpp
+++ src/tests.cpp
@@ -2,49 +2,29 @@
 #include <math.h>
 #include "tests.h"
 
-int main()
+//! Prints number of solutions.
+static void print_num_of_sols(num_of_solutions_t sol_num)
 {
-        make_test(0, 0, 0, 0, 0, INF_SOL);
-        make_test(0, 0, 2.5, 0, 0, NO_SOL);
-        make_test(0, -3.65, 0, 0, 0, ONE_SOL);
-        make_test(0, 5, -10, 0, 2, ONE_SOL);
-        make_test(2, 0, -9, -2.1213203, 2.1213203, TWO_SOL);
-        make_test(5.63, 2.96, 1.3, 0, 0, NO_SOL);
-        make_test(1, 2, 1, -1, 0, ONE_SOL);
-
-        return 0;
+        switch (sol_num) { 
+                case NO_SOL:
+                        printf("0.\n");
+                        break;
+                case ONE_SOL:
+                        printf("1.\n");
+                        break;
+                case TWO_SOL:
+                        printf("2.\n");
+                        break;
+                case INF_SOL:
+                        printf("Infinite.\n");
+                        break;
+                default: 
+                        printf("Error.\n");
+        }
 }
 
-void make_test(double coef_a, double coef_b, double coef_c,
-               double sol1, double sol2, num_of_solutions_t number_sols)
-{
-        quadra_t equation;
-        answer ans;
-
-        equation.a_coef = coef_a;
-        equation.b_coef = coef_b;
-        equation.c_coef = coef_c;
-
-        ans.solution1 = sol1;
-        ans.solution2 = sol2;
-        ans.sol_num = number_sols;
-
-        solve_equation(&equation);
-        sort_solutions(&equation);
-        test_msg(&equation, &ans, check(&equation, &ans));
-}
-
-bool check(quadra_t* equation, answer* ans)
-{
-        assert(equation);
-        assert(ans);
-
-        return (are_equal(ans->solution1, equation->solution1) &&
-                are_equal(ans->solution2, equation->solution2) &&
-                ans->sol_num == equation->sol_num);
-}
-
-void test_msg(quadra_t* equation, answer* ans, bool status)
+//! Prints the result of comparison.
+static void test_msg(quadra_t* equation, answer* ans, bool status)
 {
         assert(equation);
         assert(ans);
@@ -69,23 +49,47 @@ void test_msg(quadra_t* equation, answer* ans, bool status)
         printf("\n");
 }
 
-void print_num_of_sols(num_of_solutions_t sol_num)
+//! Returns true if the expected values align with recieved.
+static bool check(quadra_t* equation, answer* ans)
 {
-        switch (sol_num) { 
-                case NO_SOL:
-                        printf("0.\n");
-                        break;
-                case ONE_SOL:
-                        printf("1.\n");
-                        break;
-                case TWO_SOL:
-                        printf("2.\n");
-                        break;
-                case INF_SOL:
-                        printf("Infinite.\n");
-                        break;
-                default: 
-                        printf("Error.\n");
-        }
+        assert(equation);
+        assert(ans);
+
+        return (are_equal(ans->solution1, equation->solution1) &&
+                are_equal(ans->solution2, equation->solution2) &&
+                ans->sol_num == equation->sol_num);
+}
+
+//! Compares given solutions with the one that program outputs.
+static void make_test(double coef_a, double coef_b, double coef_c,
+               double sol1, double sol2, num_of_solutions_t number_sols)
+{
+        quadra_t equation;
+        answer ans;
+
+        equation.a_coef = coef_a;
+        equation.b_coef = coef_b;
+        equation.c_coef = coef_c;
+
+        ans.solution1 = sol1;
+        ans.solution2 = sol2;
+        ans.sol_num = number_sols;
+
+        solve_equation(&equation);
+        sort_solutions(&equation);
+        test_msg(&equation, &ans, check(&equation, &ans));
+} 
+
+int main()
+{
+        make_test(0, 0, 0, 0, 0, INF_SOL);
+        make_test(0, 0, 2.5, 0, 0, NO_SOL);
+        make_test(0, -3.65, 0, 0, 0, ONE_SOL);
+        make_test(0, 5, -10, 0, 2, ONE_SOL);
+        make_test(2, 0, -9, -2.1213203, 2.1213203, TWO_SOL);
+        make_test(5.63, 2.96, 1.3, 0, 0, NO_SOL);
+        make_test(1, 2, 1, -1, 0, ONE_SOL);
+
+        return 0;
 }
 
--- src/tests.h
+++ src/tests.h
@@ -10,14 +10,4 @@ struct answer {
         num_of_solutions_t sol_num = NO_SOL;
 };
 
-//! Returns true if the expected values align with recieved.
-bool check(quadra_t* equation, answer* ans);
-//! Prints the result of comparison.
-void test_msg(quadra_t* equation, answer* ans, bool status);
-//! Compares given solutions with the one that program outputs.
-void make_test(double coef_a, double coef_b, double coef_c,
-               double sol1, double sol2, num_of_solutions_t number_sols);
-//! Prints number of solutions.
-void print_num_of_sols(num_of_solutions_t sol_num);
-
 #endif // TESTS_H
